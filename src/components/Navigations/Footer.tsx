2befee389fda43edff436d18e9a4f71942e8ef6a
unknown <abdulraufyuinis@gmail.com>
2025-10-21 16:47:13 +0300

    Initial commit

---
+++ src/components/Navigations/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center p-5 text-white">
+      
+    </footer>
+  );
+};
+
+export default Footer;
